b73663dd0d4a5c950f8467df62b702b443c22275
Phat Tran <34192355+chuoibuoituoi@users.noreply.github.com>
2019-12-02 10:50:52 -0500

    change client.py to have all function from client_try.py, add encryption from server sending back to client and client decrypt it

--- client.py
+++ client.py
@@ -1,33 +1,97 @@
 from tkinter import *
 from tkinter import Entry, font
-from socket import AF_INET,socket, SOCK_STREAM
+from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 from time import sleep
+import socket
+import hashlib
+import itertools
+import sys
 import os
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import DES, PKCS1_OAEP
+
 
 class Client():
     def __init__(self, top):
-        self.client_socket = socket(AF_INET, SOCK_STREAM)
 
-        #self.HOST = '127.0.0.1'
+        KEY_SIZE = 8
 
-        self.HOST = 'ec2-54-173-78-53.compute-1.amazonaws.com'
+        # Connecting animation, set up socket
+        self.done = False
+        self.client_socket = socket.socket(AF_INET, SOCK_STREAM)
+
+        self.HOST = '127.0.0.1'
+
+        # self.HOST = 'ec2-54-173-78-53.compute-1.amazonaws.com'
         self.PORT = 9999
 
         self.BUFF = 1024
         self.ADDR = (self.HOST, self.PORT)
 
-        print("trying to connect")
         self.client_socket.connect(self.ADDR)
-        print("Connected")
 
+        thread_load = Thread(target=self.animate)
+        thread_load.start()
+
+        sleep(4)
+        self.done = True
+
+        # generate private & public key
+        random_generator = Random.new().read
+        key = RSA.generate(1024, random_generator)
+        public = key.publickey().exportKey()
+        private = key.exportKey()
+
+
+        # hashing the public key
+        hash_object = hashlib.sha1(public)
+        hex_digest = hash_object.hexdigest()
+
+        # send public key to server
+        self.client_socket.send(public)
+
+        # receive server answer
+        # send hex_digest for server confirmation
+        confirm = self.client_socket.recv(1024).decode('utf8')
+        if confirm == "YES":
+            print("\nconfirmed\n")
+            self.client_socket.send(bytes(hex_digest, 'utf8'))
+
+        # Receive server public key:
+        getpbk = self.client_socket.recv(1024).decode('utf8')
+        print("\nServer public key=\n" + getpbk)
+
+        # Store server public key
+        server_pk = RSA.importKey(getpbk)
+
+        # Generate session key and send to server
+        if getpbk != "":
+            print("\n Making session key.\n")
+            self.key_56 = os.urandom(KEY_SIZE)
+            print("\nLength of IV = " + str(len(self.key_56)))
+            print("\nSession key = " + str(self.key_56))
+
+            # encrypt session key with server public key
+            cipher_rsa = PKCS1_OAEP.new(server_pk)
+            enc_sess_key = cipher_rsa.encrypt(self.key_56)
+            print("\nEncrypted once session key = " + str(enc_sess_key))
+
+            self.client_socket.send(enc_sess_key)
+            print("\nHandshake complete.\n")
+
+            # sending name to server
+            self.alias = input("\nYour Name: ")
+            self.client_socket.send(bytes(self.alias, 'utf8'))
+
+        # Start GUI
         self.top = top
 
         self.messages_frame = Frame(self.top)
         myFont = font.Font(family='Helvetica', size=int(x / 70))
 
-        receive_thread = Thread(target=self.receive)
-        receive_thread.start()
+
 
         self.my_msg = StringVar()
         self.my_msg.set("Enter message...")
@@ -41,6 +105,8 @@ class Client():
         self.msg_list.pack(side=TOP, fill=BOTH, expand=1)
         self.messages_frame.pack(side=RIGHT, fill=BOTH, expand=1)
 
+        Thread(target=self.receive).start()
+
         # User input field and entry button
         entry_field = Entry(self.messages_frame, textvariable=self.my_msg, font=myFont, insertbackground='#c8c9cb',
                             bg='#484c52', fg='#c8c9cb')
@@ -49,37 +115,71 @@ class Client():
             str_msg = self.my_msg.get()
         else:
             str_msg = ""
-        entry_field.bind("<Return>", lambda send: (self.send(str_msg), sleep(.1), entry_field.delete('0', 'end')))
+
+        # Enter button for sending
+        entry_field.bind("<Return>",
+                         lambda send: (self.send(str_msg, self.key_56), sleep(.1), entry_field.delete('0', 'end')))
         entry_field.pack(side=LEFT, fill=BOTH, expand=1)
-        # Enter button
-        send_button = Button(self.messages_frame, font=myFont, text="Send", command=lambda: self.send(str_msg),
+
+        # Send button
+        send_button = Button(self.messages_frame, font=myFont, text="Send",
+                             command=lambda: self.send(str_msg, self.key_56),
                              bg='#484c52', fg='#c8c9cb')
         send_button.pack(ipadx=5, ipady=5, side=RIGHT, fill=BOTH)
 
-    def send(self, msg, event=None):
+    def send(self, msg, key, event=None):
+        if msg == "QUIT":
+            os._exit(0)
+
         check_msg = self.my_msg.get()
         if check_msg != "Enter message...":
             msg = check_msg
-            print(msg)
+            msg = self.alias + ": " + msg
+            print("Plain message from server: " + msg)
         sleep(.5)
-        self.client_socket.send(bytes(msg, 'utf8'))
-        if msg == "QUIT":
-            os._exit(0)
+
+
+        cipher = DES.new(key, DES.MODE_CBC, key)
+        ciphertext = cipher.encrypt(self.padding(msg))
+        print("Encrypted message to server: " + str(ciphertext))
+        self.client_socket.send(ciphertext)
 
     def receive(self):
         while True:
             try:
-                msg = self.client_socket.recv(self.BUFF).decode('utf8')
-                self.msg_list.insert(END, msg)
+                msg = self.client_socket.recv(self.BUFF)
+                print("Encrypted message from server: " + str(msg))
+                d_Msg = self.decrypt(msg, self.key_56)
+                print("Plain message from server: " + str(d_Msg))
+                Pr_Msg = d_Msg[4:-2]
+                self.msg_list.insert(END, Pr_Msg)
             except OSError:  # Possibly client has left the chat.
                 break
 
+    def animate(self):
+        for c in itertools.cycle(['....', '.......', '..........', '............']):
+            if self.done:
+                break
+            sys.stdout.write('\rCONFIRMING CONNECTION TO SERVER ' + c)
+            sys.stdout.flush()
+            sleep(0.1)
+
+    def padding(self, s):
+        return s + ((8 - len(s) % 8) * '`')
+
+    def remove_padding(self, s):
+        return s.replace('`', '')
+
+    def decrypt(self, msg, key):
+        des_cipher = DES.new(key, DES.MODE_CBC, key)
+        plaintext = self.remove_padding(str(des_cipher.decrypt(msg)))
+        return plaintext
 
 if __name__ == "__main__":
     root = Tk()
     root.title("CNT5412")
-    x = int(root.winfo_screenwidth()/1.5)
-    y = int(root.winfo_screenwidth()/2.67)
+    x = int(root.winfo_screenwidth() / 1.5)
+    y = int(root.winfo_screenwidth() / 2.67)
     root.geometry(str(x) + 'x' + str(y))
     Client(root)
     root.mainloop()
--- serverencrypt.py
+++ serverencrypt.py
@@ -1,10 +1,7 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from time import sleep
-import hashlib
 import os
-import signal
-import threading
 import hashlib
 import Crypto.Cipher.DES as DES
 from Crypto.PublicKey import RSA
@@ -15,6 +12,7 @@ from Crypto.Cipher import PKCS1_OAEP
 BLOCK_SIZE = 16
 KEY_SIZE = 8
 DES_mode = DES.MODE_CBC
+
 
 # Generate private & public RSA server keys
 random_generator = Random.new().read
@@ -31,9 +29,10 @@ priv_key_obj = RSA.importKey(private_key)
 # ONLINE_USERS = {client socket: client_name}
 ADDRESS = {}
 ONLINE_USERS = {}
+skey_l = {}
 
 HOST = '127.0.0.1'
-PORT = 9983
+PORT = 9999
 BUFF = 1024
 ADDR = (HOST, PORT)
 
@@ -41,7 +40,8 @@ ADDR = (HOST, PORT)
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
-#Accept client and take in username
+
+# Accept client and take in username
 # Exchanges public keys, verifies integrity, allows connection
 def accept_connections():
     while 1:
@@ -49,7 +49,7 @@ def accept_connections():
         print("{}:{} connected.". format(client_address[0], client_address[1]))
         ADDRESS[client_socket] = client_address
 
-        print("Waiting for public key & public key hash\n")
+        print("Waiting for client's public key & public key hash\n")
         # Obtains client's public key
         getpbk = client_socket.recv(BUFF)
         print(getpbk)
@@ -63,8 +63,8 @@ def accept_connections():
             print(getpbk)
             client_socket.send(bytes("YES", 'utf8'))
             gethash = client_socket.recv(BUFF).decode('utf8')
-            print("\nHash of public key=" + gethash)
-
+            print("\nHash of public key = " + gethash)
+            print()
 
         # Confirms integrity of client public key:
         if hex_digest == gethash:
@@ -72,16 +72,18 @@ def accept_connections():
 
             # Receive session key from client and decrypt
             enc_key = client_socket.recv(BUFF)
-            print("\nEncrypted sess_key="+str(enc_key))
+            print("*************************")
+            print("Encrypted sess_key="+str(enc_key))
 
             if enc_key != "":
                 cipher_rsa = PKCS1_OAEP.new(RSAkey)
                 dec_key = cipher_rsa.decrypt(enc_key)
-                print("\nDecrypted sess_key="+str(dec_key))
-
+                print("Decrypted sess_key="+str(dec_key))
+                print("***************************")
                 # Takes in client name and starts thread
                 username = client_socket.recv(BUFF)
                 ONLINE_USERS[client_socket] = username
+                skey_l[username] = dec_key
                 Thread(target=handle_client, args=(client_socket, dec_key)).start()
 
 
@@ -91,11 +93,10 @@ def accept_connections():
 
 
 def decrypt(msg, key):
-    # decrypts messages received from client
-    print("\n[!] Encrypted message from client: " + str(msg))
     des_cipher = DES.new(key, DES.MODE_CBC, key)
     plaintext = remove_padding(str(des_cipher.decrypt(msg)))
     return plaintext
+
 
 def padding(s):
     return s + ((8 - len(s) % 8) * '`')
@@ -107,35 +108,37 @@ def remove_padding(s):
 
 # decode client messages
 def handle_client(client, key):
-    name = ONLINE_USERS[client]
-    # welcome = "Welcome,{}! Type QUIT to exit.".format(name)
-    # msg = "{} connected.".format(name)
-    # client.send(bytes(welcome,'utf8'))
-    # client.send(bytes(welcome,'utf8'))
-    # client.send(bytes(msg, 'utf8'))
+    name = ONLINE_USERS[client].decode('utf8')
+
     sleep(.5)
-    # broadcast(msg)
+
     while 1:
         ciphertext = client.recv(BUFF)
+
         plaintext = decrypt(ciphertext, key)
-        print("\n[!] Plaintext message from client: " + str(plaintext))
+
+        print("Cipher message from client: " + str(ciphertext))
+        print("Plain message from client: " + str(plaintext))
+
+        CBCencrypt = DES.new(skey_l[name.encode('utf8')], DES.MODE_CBC, skey_l[name.encode('utf8')])
+        e_msg = CBCencrypt.encrypt(padding(plaintext))
+
         if plaintext != "QUIT":
-            broadcast(str(ciphertext), name + ": ")
-            # broadcast(msg, name + ": ")
+            print("Encrypted message to client: " + str(e_msg))
+            broadcast(e_msg)
         else:
             close_connection(client)
             break
 
+
 #Broadcast msg to chat room
-#Prefix is (name + ": ")
-def broadcast(msg, prefix=""):
-    sent_message = "{}{}".format(prefix, msg)
-    print("\n[!] Your plaintext message \n" + sent_message)
-    try:
-        for client in ONLINE_USERS:
-            client.send(bytes(sent_message, 'utf8'))
-    except:
-        pass
+#Prefix is (name + ":")
+
+def broadcast(msg):
+    for client in ONLINE_USERS:
+        client.send(msg)
+
+
 
 def close_connection(client):
     client.send(bytes("QUIT", 'utf8'))
